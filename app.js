8495ec2236690a90bbed418a5de8ea49cd19a377
Yuki Shichiku <yuuki.79@gmail.com>
2014-06-25 04:19:52 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// TODO local web socket server
+var io = require('socket.io-client'); // TODO こいつを使う
+var WEBSOCKET_PORT = 2000;
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: WEBSOCKET_PORT });
+wss.on('connection', function(ws) {
+  console.log("[WS] :: A new websocket connection was made!");
+  ws.on('message', function(msg){
+    console.log('received: %s', msg);
+    // TODO emit start from socket.io-client
+  });
+});
+
+module.exports = app;
